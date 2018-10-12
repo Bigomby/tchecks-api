ad26769813476cfe8f3b58f4a00cd3cc79668089
Diego Fernández Barrera <bigomby@gmail.com>
2018-10-12 19:28:53 +0200

    :tada: Initial commit
    Bootstrap project

---
+++ src/modules/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UsersModule } from 'modules/users/users.module';
+import { EntriesModule } from 'modules/entries/entries.module';
+import { AuthModule } from 'modules/auth/auth.module';
+import { TeachersModule } from 'modules/teachers/teachers.module';
+
+const ormconfig = require('../../ormconfig.json');
+
+ormconfig.host = process.env.DB_HOST || ormconfig.host;
+ormconfig.port = process.env.DB_PORT || ormconfig.port;
+ormconfig.username = process.env.DB_USER || ormconfig.username;
+ormconfig.password = process.env.DB_PASSWORD || ormconfig.password;
+ormconfig.database = process.env.DB_NAME || ormconfig.database;
+ormconfig.synchronize = process.env.NODE_ENV !== 'production';
+
+@Module({
+  imports: [
+    AuthModule,
+    UsersModule,
+    TeachersModule,
+    EntriesModule,
+    TypeOrmModule.forRoot(ormconfig),
+  ],
+})
+export class AppModule {}

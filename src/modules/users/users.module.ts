357f7811efe82985c4fab744786294cebe71903f
Diego Fernández Barrera <bigomby@gmail.com>
2018-11-19 10:13:52 +0100

    :wrench: Various improvements on user module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,12 +5,13 @@ import { UsersController } from 'modules/users/users.controller';
 import { UsersService } from 'modules/users/users.service';
 import { UserEntity } from 'modules/users/user.entity';
 import { AuthModule } from 'modules/auth/auth.module';
-import { AttachUserInterceptor } from 'modules/users/attach-user.interceptor';
+import { AttachUser } from 'modules/users/interceptors/attach-user.interceptor';
+import { RemoveUser } from 'modules/users/interceptors/remove-user.interceptor';
 
 @Module({
   imports: [AuthModule, TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
-  providers: [AttachUserInterceptor, UsersService],
-  exports: [AttachUserInterceptor, UsersService],
+  providers: [AttachUser, RemoveUser, UsersService],
+  exports: [AttachUser, UsersService],
 })
 export class UsersModule {}
